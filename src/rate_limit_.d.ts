a618595c12fe343296b342b035ca3d024f62455a
Brian Takita <brian.takita@gmail.com>
2022-03-23 01:28:17 -0500

    rate_limit_: feat: + <Out> generic

--- src/rate_limit_.d.ts
+++ src/rate_limit_.d.ts
@@ -2,7 +2,7 @@
  * Rate limit function factory.
  * @link {@see https://www.matteoagosti.com/blog/2013/01/22/rate-limiting-function-calls-in-javascript/}
  */
-export declare function rate_limit_(ops_max:number, interval:number, allow_bursts?:boolean):rate_limit__T;
-export declare type rate_limit__T = (fn:rate_limit_fn_T)=>Promise<void>;
-export declare type rate_limit_fn_T = ()=>Promise<void>;
+export declare function rate_limit_<Out>(ops_max:number, interval:number, allow_bursts?:boolean):rate_limit__T<Out>;
+export declare type rate_limit__T<Out> = (fn:rate_limit_fn_T<Out>)=>Promise<Out>;
+export declare type rate_limit_fn_T<Out> = ()=>Promise<Out>;
 export { rate_limit_ as _rate_limit, }
